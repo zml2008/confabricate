c1e0ffa1b6c19f94cd536f5de21f2837d46a4886
Zach Levis <zml@aoeu.xyz>
2020-04-09 01:19:04 -0700

    build: Set up publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,15 @@
 plugins {
     id("fabric-loom") version "0.2.6-SNAPSHOT"
     id("com.github.hierynomus.license") version "0.15.0"
-    // Apply the java-library plugin for API and implementation separation.
     `java-library`
+
+    // Publication
+    `maven-publish`
+    id("com.jfrog.bintray") version "1.8.5"
+    id("org.ajoberstar.grgit") version "4.0.2"
 }
 
+val versionBase = "1.0-SNAPSHOT"
 val versionMinecraft = ext["version.minecraft"] as String
 val versionMappings = ext["version.mappings"] as String
 val versionLoader = ext["version.loader"] as String
@@ -12,7 +17,7 @@ val versionFabricApi = ext["version.fabricApi"] as String
 val versionConfigurate = ext["version.configurate"] as String
 
 group = "ca.stellardrift"
-version = "1.0-SNAPSHOT+$versionConfigurate"
+version = "$versionBase+$versionConfigurate"
 
 minecraft {
     refmapName = "${rootProject.name.toLowerCase()}-refmap.json"
@@ -34,10 +39,21 @@ repositories {
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
+    withJavadocJar()
+    withSourcesJar()
 }
 
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes("Specification-Name" to "Configurate",
+        "Specification-Version" to versionConfigurate,
+        "Implementation-Name" to project.name,
+        "Implementation-Version" to versionBase)
+    }
 }
 
 tasks.processResources {
@@ -71,5 +87,86 @@ dependencies {
     // listOf("commands-v0", "api-base").forEach {
     //     implementationInclude("net.fabricmc.fabric-api:fabric-$it:$versionFabricApi")
     // }
+}
+
+val publicationId = "maven"
+publishing {
+    publications {
+        register(publicationId, MavenPublication::class) {
+            artifact(tasks.jar.get()) {
+                classifier = "dev"
+            }
+            artifact(tasks.remapJar.get())
+
+            artifact(tasks.getByName("sourcesJar")) {
+                builtBy(tasks.remapSourcesJar.get())
+            }
+            artifact(tasks.getByName("javadocJar"))
+            //from(components["java"]) // doesn't work on fabric
+
+            pom {
+                description.set(project.ext["longDescription"] as String)
+
+                developers {
+                    developer {
+                        name.set("zml")
+                        email.set("zml at stellardrift dot ca")
+                    }
+                }
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+
+                scm {
+                    url.set("https://github.com/zml2008/confabricate")
+                    connection.set("scm:git:https://github.com/zml2008/confabricate.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/zml2008/confabricate.git")
+                }
+
+                issueManagement {
+                    system.set("GitHub Issues")
+                    url.set("https://github.com/zml2008/confabricate/issues")
+                }
+            }
+        }
+
+    }
+}
+bintray {
+    user = findProperty("bintrayUser") as String? ?: System.getenv("BINTRAY_USER")
+    key = findProperty("bintrayKey") as String? ?: System.getenv("BINTRAY_KEY")
+    pkg.apply {
+        repo = "stellardrift-repo"
+        name = project.name
+        setLicenses("Apache-2.0")
+        vcsUrl = "https://github.com/zml2008/confabricate.git"
+        version.apply {
+            val tagRef = grgit.repository.jgit.tagList().call().firstOrNull()
+            val tag = grgit.resolve.toTag(tagRef?.name)
+            name = project.version as String
+            vcsTag = tag?.name
+            desc = tag?.fullMessage
+            released = tag?.commit?.dateTime?.toString()
+        }
+    }
+    setPublications(publicationId)
+}
+val requireClean by tasks.registering {
+    doLast {
+        if (!grgit.status().isClean) {
+            throw TaskExecutionException(this, Exception("Source root must be clean! Make sure your changes are committed."))
+        }
+    }
+}
+
+tasks.bintrayUpload {
+    dependsOn(requireClean)
+    onlyIf {
+        !(version as String).contains("SNAPSHOT")
+    }
 }
 
