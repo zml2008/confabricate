7fac98985ac289481010c36d3d359ce1c0a6c3fa
zml <zml@stellardrift.ca>
2020-05-26 22:01:22 -0700

    Fix RegistrySerializer to actually work

--- src/main/java/ca/stellardrift/confabricate/typeserializers/RegistrySerializer.java
+++ src/main/java/ca/stellardrift/confabricate/typeserializers/RegistrySerializer.java
@@ -51,6 +51,9 @@ public class RegistrySerializer<T> implements TypeSerializer<T> {
         }
 
         Identifier ident = registry.getId(obj);
-
+        if (ident == null) {
+            throw new ObjectMappingException("Unknown identifier " + ident + " for registry " + registry);
+        }
+        IdentifierSerializer.toNode(ident, value);
     }
 }
