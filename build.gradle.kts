f599dded7848f47f9393846fe14f41c8d3355c90
Zach Levis <zml@aoeu.xyz>
2020-06-23 07:12:09 -0700

    Release version 1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("ca.stellardrift.opinionated.publish") version "3.1"
 }
 
-val versionBase = "1.2-SNAPSHOT"
+val versionBase = "1.2"
 val versionMinecraft: String by project
 val versionMappings: String by project
 val versionLoader: String by project
