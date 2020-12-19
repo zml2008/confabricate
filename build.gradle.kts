6d41d59448bb40985e51413bd7e8e1b311c1755c
Zach Levis <zml@aoeu.xyz>
2020-12-19 15:35:12 -0800

    Release version 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("net.kyori.indra.publishing.bintray") version "1.2.1"
 }
 
-val versionBase = "2.0-SNAPSHOT"
+val versionBase = "2.0.0"
 val versionMinecraft: String by project
 val versionMappings: String by project
 val versionLoader: String by project
