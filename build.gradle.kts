b007b752f1ce12cb92e7eee30ef31f1742a5e9c8
zml <zml@stellardrift.ca>
2021-06-06 11:33:53 -0700

    1.17-rc1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ import ca.stellardrift.build.common.stellardriftSnapshots
 
 plugins {
     id("net.ltgt.errorprone") version "2.0.1"
+    id("fabric-loom") version "0.8.+"
     id("ca.stellardrift.opinionated.fabric") version "5.0.0"
     id("net.kyori.indra.publishing.sonatype") version "2.0.5"
 }
@@ -81,7 +82,7 @@ indra {
     apache2License()
 
     javaVersions {
-        testWith(8, 11, 16)
+        target(16)
     }
 
     configurePublications {
